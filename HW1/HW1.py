90bfa2197a9b2bd762d46d8d951e3b62624cabb4
Luke Dercher <luke.dercher@gmail.com>
2018-08-26 14:56:37 -0500

    fixed python summation functions, err in adoc sol, added python output txt

--- HW1/HW1.py
+++ HW1/HW1.py
@@ -1,14 +1,20 @@
-def sum(n):
+def sum(ll, n):
     sum = 0
-    for i in range (0,n):
-        sum += i
+    for i in range (ll,n + 1):
+        sum = sum + i
     return sum
 
-def sum_squared(n):
+def sum_squared(ll, n):
     sum_squared = 0
-    for i in range (0,n):
+    for i in range (ll,n + 1):
         sum_squared += (i*i)
     return sum_squared
+
+def sum_const(ll, n, const):
+    const_sum = 0
+    for i in range (ll, n +1):
+        const_sum += const
+    return const_sum
 
 def S(N):
     A = []
@@ -24,15 +30,13 @@ def S(N):
 
 
 def main():
-    print ("sum from 1 to 100 = %s \n\n" % sum(100))
-    print ("sum squared from 1 to 100 = %s \n\n" % sum_squared(100))
-    print ("sum of k squared plus k plus 1 from 1 to 123 = %s \n\n" % (sum_squared(123) + sum(123) + 1))
+    print ("sum from 1 to 100 = %s \n\n" % sum(0,100))
+    print ("sum squared from 1 to 100 = %s \n\n" % sum_squared(0,100))
+    print ("sum of k squared plus k plus 1 from 12 to 123 = %s \n\n" % (sum_squared(12,123) + sum(12,123) + sum_const(12,123,1)))
     print ('S(10) =')
     print (S(10))
     print ('\n\nS(100) =')
     print (S(100))
-
-    #print (" S(100) = ".join(map(str, S(100))))
 
 
 main()
