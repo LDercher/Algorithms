e64d845501a886cb7d37606d3bfde525555fd2e0
luke <luke.dercher@gmail.com>
2018-11-04 09:37:22 -0600

    cc 1st attempt

--- HW11/homework11.py
+++ HW11/homework11.py
@@ -1,6 +1,7 @@
 # imports {{{1
 from __future__ import division
 from random import randrange
+import math
 #---------------------------------------------------------------------------}}}1
 
 def rand_perm(size):  # {{{
@@ -31,17 +32,48 @@ def credit_card(L): # {{{
   # You should return the element if there is one as well as a count of the
   # number of times it occurs in L. If there is no such element, return None, 0.
 
-  return None, 0  # if there is no majority element
+
+
+  return find_majority_elem(L,len(L))[0]  # if there is no majority element
+
+def union(a, b):
+    """ return the union of two lists """
+    return list(set(a) | set(b))
+
+def find_majority_elem(L,n):
+
+  if n <= 2:
+    return L
+    
+  candidates = union(find_majority_elem(L[:int(math.floor(n/2))],int(math.floor(n/2))),find_majority_elem(L[:int(math.ceil(n/2))],int(math.ceil(n/2))))
+
+  print "length of candidates ",len(candidates), "length of L", len(L) 
+
+  majority = []
+  for i in range(len(candidates)):
+    count = 0
+    j = 1
+    while j < n:
+      k = L[j]
+      l = candidates[i]
+      if L[j] == candidates[i]:
+        count +=1
+      j +=1
+
+    if count >= math.ceil(n/2):
+      majority.append((candidates[i],count))
+
+    return majority
 #----------------------------------------------------------------------------}}}
 
 # test your credit_card() solution using something like this
-#for _ in xrange(10**4):
-#  v = randrange(2,30)
-#  s = randrange(2*v-1,v**2)
-#  R = rand_cards(s,v)
-#  A, count = credit_card_linear(R)
-#  if A != v:
-#    print "whoops"
-#    print R
-#    print A
-#    break
+for _ in xrange(10**4):
+  v = randrange(2,30)
+  s = randrange(2*v-1,v**2)
+  R = rand_cards(s,v)
+  A, count = credit_card(R)
+  if A != v:
+    print "whoops"
+    print R
+    print A
+    break
